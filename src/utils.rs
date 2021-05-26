2a2716acf094f188a083f518d8b6fe4906bab6c7
Kristopher Wuollett <kristopher.wuollett@gmail.com>
2021-05-26 12:01:57 -0400

    Add tick duration timer

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
 extern crate web_sys;
 
+use web_sys::console;
+
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 #[macro_export]
 macro_rules! log {
@@ -17,4 +19,21 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
 }
